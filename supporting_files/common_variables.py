490d39bcd75c6b6d4da55fc6825563b4177f2659
siddhanth <siddhanthjain95@gmail.com>
2025-04-28 14:34:09 +0530

    updating the test cases for delivery post method

--- supporting_files/common_variables.py
+++ supporting_files/common_variables.py
@@ -4,5 +4,8 @@ class CommonVariables:
 
     def __init__(self):
         self.myurl = "https://jsonplaceholder.typicode.com/posts"
-        self.integrationurl = "https://cf-test.lab.amagi.com/api/deliveries"
-        self.url = "https://api-now3.secure.amagi.tv/api/deliveries"
+        # TODO INTEGRATION url
+        self.url = "https://cf-test.lab.amagi.com/api/deliveries"
+        #TODO PROD url
+        #self.url = "https://api-now3.secure.amagi.tv/api/deliveries"
+
--- tests/test_api_mainfile.py
+++ tests/test_api_mainfile.py
@@ -12,15 +12,15 @@ class TestAapiVvod:
     def setup_support(self, support_methods_fixture):
         self.support = support_methods_fixture
 
-    @pytest.mark.asyncio
-    async def test_get_post(self):
-        response = await self.support.call_get_method()
-        assert response.status_code == 200
-
-    @pytest.mark.asyncio
-    async def test_post(self):
-        response = await self.support.call_post_method()
-        assert response.status_code == 201
+    # @pytest.mark.asyncio
+    # async def test_get_post(self):
+    #     response = await self.support.call_get_method()
+    #     assert response.status_code == 200
+    #
+    # @pytest.mark.asyncio
+    # async def test_post(self):
+    #     response = await self.support.call_post_method()
+    #     assert response.status_code == 201
 
     @pytest.mark.asyncio
     async def test_delivery_post_api(self):
