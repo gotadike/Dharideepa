cd31fbb7cb06114359e7e83fb46b368a9a9251bf
siddhanth <siddhanthjain95@gmail.com>
2025-06-20 17:27:57 +0530

    updating the test cases for delivery post method

--- supporting_files/common_variables.py
+++ supporting_files/common_variables.py
@@ -5,7 +5,7 @@ class CommonVariables:
     def __init__(self):
         self.myurl = "https://jsonplaceholder.typicode.com/posts"
         # TODO INTEGRATION url
-        self.url = "https://cf-test.lab.amagi.com/api/deliveries"
+        #self.url = "https://cf-test.lab.amagi.com/api/deliveries"
         #TODO PROD url
-        #self.url = "https://api-now3.secure.amagi.tv/api/deliveries"
+        self.url = "https://api-now3.secure.amagi.tv/api/deliveries"
 
